3d648665895ca77bf1cd35abe3046ab4f4f7e105
Wahyu Krishna <hanzo180895@gmail.com>
2021-03-03 23:34:32 +0700

    Tugas 12 JS
    Tugas Niomic

---
+++ tugas12.js
@@ -0,0 +1,13 @@
+const tugas12 = () => {
+    var buah = ['Pisang','Jeruk','Apel','Mangga']
+    console.log(buah);
+    buah.pop();
+    console.log(buah);
+    buah.shift();
+    return buah
+}
+
+console.log(tugas12());
+
+
+
